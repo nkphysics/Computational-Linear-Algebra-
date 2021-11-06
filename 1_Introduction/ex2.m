453a2c29e6e7a5347aa3087996c716bab9dbf70c
Tsar Bomba Nick <njkuechel@protonmail.com>
2021-11-06 10:14:45 -0600

    Minor Comment Fixes

--- 1_Introduction/ex2.m
+++ 1_Introduction/ex2.m
@@ -7,7 +7,7 @@ clear all;
 # Generates a random 1000x1000 matrix with non-zero ints up to 1,000,000
 A = randi(1000000, 1000, 1000);
 
-# Creates a 100-d column vector of zeros
+# Creates a 1000-d column vector of zeros
 b = zeros(1,1000)';
 # Makes the 6th element of the b vector 3
 b(69) = 12;
