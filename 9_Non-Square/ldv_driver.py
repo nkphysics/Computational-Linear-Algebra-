e73f557e5824849c20f08181d6089b4c4f328e4b
Tsar Bomba Nick <njkuechel@protonmail.com>
2022-04-29 09:04:32 -0600

    9 Non-square changes to lin_alg SGE LU and LDV for compatability with non square matrices

---
+++ 9_Non-Square/ldv_driver.py
@@ -0,0 +1,19 @@
+# Computational Linear Algebra Ep#9 Non-quare LDV Decomposition
+# By: Nick Space Cowboy
+
+import numpy as np
+from lin_alg import lin_alg
+
+if __name__ == "__main__":
+	A = np.array(np.random.randint (0,100,(2, 5)), dtype=np.float64)
+	print("A: ")
+	print(A)
+	L, D, V = lin_alg.LDV(A)
+	print("L:")
+	print(L)
+	print("D:")
+	print(D)
+	print("V:")
+	print(V)
+	print("Check:")
+	print((A - np.dot(np.dot(L, D), V)).round())
---
+++ 9_Non-Square/lu_driver.py
@@ -0,0 +1,20 @@
+# Computational Linear Algebra Ep#9 Non-Square Lu Decomposition
+# By: Nick Space Cowboy
+
+import numpy as np
+from lin_alg import lin_alg
+
+if __name__ == "__main__":
+	# generate A matrix
+	A = np.array(np.random.randint (1,100,(2,4)), dtype=np.float64)
+	print("A: ")
+	print(A)
+	# perform LU decomposition with our LinAlg LU function
+	L, U = lin_alg.LU(A)
+	print("L: ")
+	print(L)
+	print("U: ")
+	print(U)
+	print("Check: ")
+	# Check that LU=A
+	print((A - np.dot(L,U)).round())
